cd068ececbbb9d80d327dfa49e65cfbdb203769a
fabio carvalho <fabio.carvalho.ext@nokia.com>
2021-04-18 12:42:07 +0100

    update

--- stackoverflow.js
+++ stackoverflow.js
@@ -1,31 +1,29 @@
 function goToSectionPage(sectionElement) {
-  sectionElement.scrollIntoView({ behavior: "smooth", block: "center" });
+  sectionElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
-
+function addCssToSelecedtSession(parentWrapSectionElements, selectedSectionElement, className) {
+  for (const section of parentWrapSectionElements.children) {
+    section.classList.remove(className);
+  }
+  selectedSectionElement.classList.add(className);
+}
 function addEventHandlerToHeaderButton(buttonId, sectionId) {
   const buttonTarget = document.getElementById(buttonId);
 
-  buttonTarget.addEventListener("click", function () {
-    const parentSectionElement = document.querySelector("main");
+  buttonTarget.addEventListener('click', () => {
+    const parentSectionElement = document.querySelector('main');
     const targetSection = document.getElementById(sectionId);
 
     goToSectionPage(targetSection);
-    addCssClassToSelecedtSession(parentSectionElement, targetSection, "selected");    
+    addCssToSelecedtSession(parentSectionElement, targetSection, 'selected');
   });
 }
 
-function addCssClassToSelecedtSession(parentWrapSectionElements, selectedSectionElement, className) {
-  for(let section of parentWrapSectionElements.children){
-    section.classList.remove(className)
-  }
-  selectedSectionElement.classList.add(className)
-}
-
-addEventHandlerToHeaderButton("headerSection1", "section1");
-addEventHandlerToHeaderButton("headerSection2", "section2");
-addEventHandlerToHeaderButton("headerSection3", "section3");
-addEventHandlerToHeaderButton("headerSection4", "section4");
-addEventHandlerToHeaderButton("headerSection5", "section5");
-addEventHandlerToHeaderButton("headerSection6", "section6");
-addEventHandlerToHeaderButton("headerSection7", "section7");
-addEventHandlerToHeaderButton("headerAbout", "about");
+addEventHandlerToHeaderButton('headerSection1', 'section1');
+addEventHandlerToHeaderButton('headerSection2', 'section2');
+addEventHandlerToHeaderButton('headerSection3', 'section3');
+addEventHandlerToHeaderButton('headerSection4', 'section4');
+addEventHandlerToHeaderButton('headerSection5', 'section5');
+addEventHandlerToHeaderButton('headerSection6', 'section6');
+addEventHandlerToHeaderButton('headerSection7', 'section7');
+addEventHandlerToHeaderButton('headerAbout', 'about');
